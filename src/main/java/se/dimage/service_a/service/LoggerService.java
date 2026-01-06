4bd02a3a3d71a024fbcc202171f5e9978bbdfcdd
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2026-01-06 03:23:03 +0100

    logging added

--- src/main/java/se/dimage/service_a/service/LoggerService.java
+++ src/main/java/se/dimage/service_a/service/LoggerService.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
-import java.net.URL;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,13 +17,30 @@ public class LoggerService {
     @Value("${service_b.url}")
     private String url;
 
-    public String getMessage() {
-        try {
-            URL url_b = new URL(url);
+    @Value("logfile")
+    private String logFileName;
 
+    public String getMessage() {
+        String result = "";
+        try {
+            RestClient client = RestClient.create(url);
+            result = client.get().retrieve().body(String.class);
+            log(result);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
-        return "";
+        return result;
+    }
+
+    public void log(String text) {
+        log.info(text);
+        try (FileWriter fw = new FileWriter(logFileName, true)) {
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.newLine();
+            bw.write(text);
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
